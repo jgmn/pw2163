41ad6951ae46481f3c5a593742a41992e6afbf81
jgmn <jesus.gerardo.mn@outlook.com>
2016-11-17 11:53:25 -0700

    JQuery y CSS

--- js/eventos.js
+++ js/eventos.js
@@ -14,9 +14,12 @@ var inicio = function()
 		  url: 'https://randomuser.me/api/',
 		  dataType: 'json',
 		  success: function(data) {
+		  	$("#imgFoto").show("slow");
+		  	$("#txtNombre").show("slow");
 		  	$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
-		  	$("#imgFoto").attr("src", data.results[0].picture.medium);
+		  	$("#imgFoto").attr("src", data.results[0].picture.large);
 		  	console.log(data.results[0].name.first+" "+data.results[0].name.last);
+		  	$("#miArticle").html("Ejemplo"); /*Cuando no sé como llenarlo utilizamos html*/
 		  }
 		});
 	}
