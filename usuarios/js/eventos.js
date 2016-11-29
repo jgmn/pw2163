b22dbeb1a33f4a0437d72f49aaf420e4129e53dd
jgmn <jesus.gerardo.mn@outlook.com>
2016-11-29 12:01:18 -0700

    Sistema de prueba no funciona

---
+++ usuarios/js/eventos.js
@@ -0,0 +1,54 @@
+var inicioUsuarios = function()
+{
+	validaUsuario = function()
+	{
+		//Extraer los datos de los input en el HTML.
+		var usuario = $("#txtUsuario").val();
+		var clave = $("#txtClave").val();
+
+		//Preparar los parámetros para AJAX.
+		var parametros = "opcion=valida"+
+						 "&usuario="+usuario+
+						 "&clave="+clave+
+						 "&id="+Math.random();
+
+
+		//Validamos que no estén vacíos.
+		if(usuario!="" && clave!="")
+		{
+			//Hacemos la petición remota.
+			$.ajax({
+				cache:false,
+				type:"POST",
+				dataType:"json",
+				url:"php/utilerias.php",
+				data: parametros,
+				success: function(response){
+					//Si todo sale bien.
+					if(response.respuesta)
+					{
+						$("#entradaUsuario").hide("slow");
+						$("nav").show("slow");
+					}
+					else
+					{
+						alert("Datos incorrectos :(");
+					}
+				},
+				error: function(xhr, ajaxOptions, thrownError){
+					//Si todo sale mal.
+				}
+			});
+
+		}
+		else
+		{
+			alert("Datos obligatorios");
+		}
+	}
+
+	$("#btnValidaUsuario").on("click", validaUsuario);
+}
+
+//Evento inicial.
+$(document).on("ready", inicioUsuarios);
---
+++ usuarios/php/utilerias.php
@@ -0,0 +1,65 @@
+<?php 
+	
+	function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+	{
+	  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+
+	  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+	  switch ($theType) {
+	    case "text":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;    
+	    case "long":
+	    case "int":
+	      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+	      break;
+	    case "double":
+	      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+	      break;
+	    case "date":
+	      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+	      break;
+	    case "defined":
+	      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+	      break;
+		}
+		
+		return $theValue;
+	}
+	
+	function validaUsuario()
+	{
+		$respuesta = false;
+		$usuario   = GetSQLValueString($_POST["usuario"], "text"); //Limpieza.
+		$clave     = GetSQLValueString($_POST["clave"], "text"); //Limpieza.
+		//Conexión a la base de datos.
+		$conexion  = mysql_connect("localhost", "root", "");
+		mysql_select_db("bd2163");
+		$consulta  = sprintf("SELECT usuario, clave FROM usuarios WHERE usuario=%s AND clave=%s LIMIT 1", $usuario, $clave);
+		$resultado = mysql_query($consulta);
+
+		//Esperamos un solo resultado.
+		if(mysql_num_rows(result) == 1)
+		{
+			respuesta = true;
+		}
+
+		//Convertir respuesta a JSON.
+		$JSON = array('respuesta' => $respuesta);
+		print json_encode($JSON);
+	}
+
+	//Menú principal.
+	$op = $_POST["opcion"];
+	
+	switch ($op) 
+	{
+		case 'valida':
+			validaUsuario();
+			break;
+		default:
+			# code...
+			break;
+	}
+?>
